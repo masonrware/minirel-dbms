63d386a3561f963fee9073189290e2eb4fb5411f
masonrware <86129546+masonrware@users.noreply.github.com>
2024-04-16 17:51:44 -0500

    adding stuff to help debug

--- page.C
+++ page.C
@@ -97,7 +97,7 @@ const Status Page::insertRecord(const Record & rec, RID& rid)
 
 	tmpRid.pageNo = curPage;
 	tmpRid.slotNo = -i; // make a positive slot number
-	cout << "100 ~!!! " << tmpRid.slotNo << endl;
+	// cout << "100 ~!!! " << tmpRid.slotNo << endl;
 	rid = tmpRid;
 
 	return OK;
