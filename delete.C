55dcf395804f9a7ab30050e4295972d8c36feadc
masonrware <86129546+masonrware@users.noreply.github.com>
2024-04-29 14:20:47 -0500

    commenting out error msg

--- delete.C
+++ delete.C
@@ -69,35 +69,35 @@ const Status QU_Delete(const string & relation,
 
 
 
-scan1 = new HeapFileScan("dummy.02", status);
-    if (status != OK) error.print(status);
-    else 
-    {
-        scan1->startScan(0, 0, STRING, NULL, EQ);
-		i = 0;
-		deleted = 0;
-		while ((status = scan1->scanNext(rec2Rid)) != FILEEOF)
-		{
-			// cout << "processing record " << i << i << endl;
-			if (status != OK) error.print(status);
-			if ((i % 2) != 0)
-			{
-				//printf("deleting record %d with rid(%d.%d)\n",i,rec2Rid. pageNo, rec2Rid.slotNo);
-				status = scan1->deleteRecord(); 
-				deleted++;
-				if ((status != OK)  && ( status != NORECORDS))
-				{
-                    cout << "err0r status return from deleteRecord" << endl;
-                    error.print(status);
-				}
-			}
-			i++;
-		}
-		if (status != FILEEOF) error.print(status);
-		cout << "deleted " << deleted << " records" << endl;
-		if (deleted != num / 2)
-            cout << "Err0r.   should have deleted " << num / 2 << " records!" << endl;
-		scan1->endScan();
-    }
-    delete scan1;
-    scan1 = NULL;
+// scan1 = new HeapFileScan("dummy.02", status);
+//     if (status != OK) error.print(status);
+//     else 
+//     {
+//         scan1->startScan(0, 0, STRING, NULL, EQ);
+// 		i = 0;
+// 		deleted = 0;
+// 		while ((status = scan1->scanNext(rec2Rid)) != FILEEOF)
+// 		{
+// 			// cout << "processing record " << i << i << endl;
+// 			if (status != OK) error.print(status);
+// 			if ((i % 2) != 0)
+// 			{
+// 				//printf("deleting record %d with rid(%d.%d)\n",i,rec2Rid. pageNo, rec2Rid.slotNo);
+// 				status = scan1->deleteRecord(); 
+// 				deleted++;
+// 				if ((status != OK)  && ( status != NORECORDS))
+// 				{
+//                     cout << "err0r status return from deleteRecord" << endl;
+//                     error.print(status);
+// 				}
+// 			}
+// 			i++;
+// 		}
+// 		if (status != FILEEOF) error.print(status);
+// 		cout << "deleted " << deleted << " records" << endl;
+// 		if (deleted != num / 2)
+//             cout << "Err0r.   should have deleted " << num / 2 << " records!" << endl;
+// 		scan1->endScan();
+//     }
+//     delete scan1;
+//     scan1 = NULL;
